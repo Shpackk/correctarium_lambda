b2b2c17461b88629f4a0dc2011daad8697ee134e
Shpackk <victorija45@gmail.com>
2021-10-27 17:21:14 +0300

    done calctime, response unix timestamp added

--- controllers/priceController.js
+++ controllers/priceController.js
@@ -5,6 +5,12 @@ module.exports.makeOrder = (req, res, next) => {
     try {
         const price = calcPrice(options)
         const time = calcTime(options)
+        res.status(201).send({
+            price,
+            time: Math.round(options.count / symbPerHour[options.language]),
+            deadline: time.deadline_unix,
+            deadline_date: time.deadline_date
+        })
     } catch (error) {
         console.log(error)
     }
@@ -25,10 +31,25 @@ function calcPrice(options) {
 
 function calcTime(options) {
     try {
-        const hoursToEdit = Math.round(options.count / symbPerHour[options.language])
+        const hoursToEdit = Math.round(options.count / symbPerHour[options.language]) < 1 ? 1 : Math.round(options.count / symbPerHour[options.language])
         const dateNow = new Date()
-        console.log(`${dateNow.getDate()}/${dateNow.getMonth() + 1}/${dateNow.getFullYear()} ${dateNow.getHours()}:${dateNow.getMinutes()}:${dateNow.getSeconds()}`)
+        dateNow.setDate(dateNow.getDate() + Math.floor(hoursToEdit / 9))
+        dateNow.setHours(dateNow.getHours() + Math.round(hoursToEdit % 9))
+        const deadLineTime = cleanUpTime(dateNow)
+        return {
+            deadline_date: `${deadLineTime.getDate()}/${deadLineTime.getMonth() + 1}/${deadLineTime.getFullYear()} ${deadLineTime.getHours()}:${deadLineTime.getMinutes()}:${deadLineTime.getSeconds()}`,
+            deadline_unix: parseInt((deadLineTime.getTime() / 1000).toFixed(0))
+        }
     } catch (error) {
-
+        throw error
     }
+}
+
+function cleanUpTime(dateNow) {
+    dateNow.setMinutes(dateNow.getMinutes() + 30)
+    if (dateNow.getHours() > 19) {
+        dateNow.setHours(dateNow.getHours() - 19 + 12)
+        dateNow.setDate(dateNow.getDate() + 1)
+    }
+    return dateNow
 }
